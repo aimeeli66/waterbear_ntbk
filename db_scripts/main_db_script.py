6fba570fda9aca2778824e5fc9eeec3519978944
aimeeli66 <41303471+aimeeli66@users.noreply.github.com>
2018-07-25 11:25:50 -0500

    Update main_db_script.py

--- db_scripts/main_db_script.py
+++ db_scripts/main_db_script.py
@@ -6,6 +6,6 @@ base_manuscript_dir = os.path.join(directory,'./')
 data_dir = os.path.join(base_manuscript_dir,'./')
 fig_dir = os.path.join(base_manuscript_dir,'./')
 
-raw_db_filename = "/home/shuting/Documents/My_test/9e1b23607ac240588ba50d6b5b9a49b5/data/contents/" \
+raw_db_filename = "/./" \
                   "hampt_rd_data.sqlite"
-db_filename = "/home/shuting/Documents/flood_data-master/db_scripts/floodData.sqlite"
+db_filename = "floodData.sqlite"
