47b34678aed27b39bf6ab22ba4c10dfcd0a47afa
aimeeli66 <41303471+aimeeli66@users.noreply.github.com>
2018-07-25 11:15:28 -0500

    Create main_db_script.py

---
+++ db_scripts/main_db_script.py
@@ -0,0 +1,11 @@
+import os
+
+
+directory = os.path.dirname(__file__)
+base_manuscript_dir = os.path.join(directory,'./')
+data_dir = os.path.join(base_manuscript_dir,'./')
+fig_dir = os.path.join(base_manuscript_dir,'./')
+
+raw_db_filename = "/home/shuting/Documents/My_test/9e1b23607ac240588ba50d6b5b9a49b5/data/contents/" \
+                  "hampt_rd_data.sqlite"
+db_filename = "/home/shuting/Documents/flood_data-master/db_scripts/floodData.sqlite"
